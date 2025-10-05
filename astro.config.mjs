e3c2a492730f81c85136e72beb6b8362fd021d5a
Amgde22 <magidembarek@gmail.com>
2025-10-05 17:50:18 +0100

    add critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,60 +3,58 @@ import icon from "astro-icon";
 import i18n from "@astrolicious/i18n";
 import sitemap from "@astrojs/sitemap";
 
+import crittersRs from "@critters-rs/astro";
+
 export default defineConfig({
   image: {
     service: passthroughImageService(),
   },
   site: "https://www.lingosource.uk", 
-  integrations: [
-    icon(),
-    i18n({
-      defaultLocale: "en",
-      locales: ["fr", "en"],
-      client: {
-        data: true,
-        paths: true,
+  integrations: [icon(), i18n({
+    defaultLocale: "en",
+    locales: ["fr", "en"],
+    client: {
+      data: true,
+      paths: true,
+    },
+    // used to localize the routes
+    pages: {
+      "/about": {
+        "fr": "/a-propos"
       },
-      // used to localize the routes
-      pages: {
-        "/about": {
-          "fr": "/a-propos"
-        },
-        "/contact":{
-          "fr":"/contact"
-        },
-        "/dedicated-agent-plans":{
-          "fr":"/forfaits-agent-dedie"
-        },
-        "/call-answering-plans":{  
-          "fr":"/forfaits-reception-d-appels"  
-        },
-        "/how-it-works":{
-          "fr":"/comment-ca-marche"
-        },
-        "/blog":{
-          "fr":"/blog"
-        },
-        "/privacy-policy":{
-          "fr":"/politique-de-confidentialite"
-        },
-        "/terms-of-service":{
-          "fr":"/conditions-d-utilisatio"
-        },
-        "/call-answering-service":{
-          "fr":"/service-reception-d-appels"
-        }
+      "/contact":{
+        "fr":"/contact"
+      },
+      "/dedicated-agent-plans":{
+        "fr":"/forfaits-agent-dedie"
+      },
+      "/call-answering-plans":{  
+        "fr":"/forfaits-reception-d-appels"  
+      },
+      "/how-it-works":{
+        "fr":"/comment-ca-marche"
+      },
+      "/blog":{
+        "fr":"/blog"
+      },
+      "/privacy-policy":{
+        "fr":"/politique-de-confidentialite"
+      },
+      "/terms-of-service":{
+        "fr":"/conditions-d-utilisatio"
+      },
+      "/call-answering-service":{
+        "fr":"/service-reception-d-appels"
       }
-    }),
-    sitemap({
-      i18n: {
-        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `"https://www.yourwebsite.com/"` will be treated as default locale, i.e. `en`
-        locales: {
-          // key/value pairs of all languages supported
-          en: 'en-US', // The `defaultLocale` value must be present in `locales` keys
-          fr: 'fr-FR',
-        },
+    }
+  }), sitemap({
+    i18n: {
+      defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `"https://www.yourwebsite.com/"` will be treated as default locale, i.e. `en`
+      locales: {
+        // key/value pairs of all languages supported
+        en: 'en-US', // The `defaultLocale` value must be present in `locales` keys
+        fr: 'fr-FR',
       },
-    }),
-  ],
+    },
+  }), crittersRs()],
 });
